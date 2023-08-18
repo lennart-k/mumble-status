dde0b9d0ccb9c3765e352b1348ea087736bc8ccb
Lennart <18233294+lennart-k@users.noreply.github.com>
2023-08-19 00:50:12 +0200

    Add initial Prometheus exporter implementation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,11 +4,12 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/lennart-k/mumble-status/pkg/status"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/lennart-k/mumble-status/pkg/status"
 )
 
 type GetStatusHandler struct {
@@ -26,6 +27,51 @@ func (h *GetStatusHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	w.Write(out)
+}
+
+type StatusExporter struct {
+	MumbleAddress     string
+	users_online      prometheus.Gauge
+	users_max         prometheus.Gauge
+	allowed_bandwidth prometheus.Gauge
+}
+
+func NewStatusExporter(mumble_address string) StatusExporter {
+	users_online := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "users_online",
+		Namespace: "mumble_server",
+	})
+	users_max := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "users_max",
+		Namespace: "mumble_server",
+	})
+	allowed_bandwidth := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      "allowed_bandwidth",
+		Namespace: "mumble_server",
+	})
+	return StatusExporter{
+		MumbleAddress:     mumble_address,
+		users_online:      users_online,
+		users_max:         users_max,
+		allowed_bandwidth: allowed_bandwidth,
+	}
+}
+
+func (s *StatusExporter) Collect(ch chan<- prometheus.Metric) {
+	status, _ := status.GetServerStatus(s.MumbleAddress)
+
+	s.users_online.Set(float64(status.UsersOnline))
+	s.users_max.Set(float64(status.UsersMax))
+	s.allowed_bandwidth.Set(float64(status.AllowedBandwidth))
+	ch <- s.users_online
+	ch <- s.users_max
+	ch <- s.allowed_bandwidth
+}
+
+func (s *StatusExporter) Describe(ch chan<- *prometheus.Desc) {
+	ch <- s.users_online.Desc()
+	ch <- s.users_max.Desc()
+	ch <- s.allowed_bandwidth.Desc()
 }
 
 func main() {
@@ -63,6 +109,10 @@ func main() {
 
 	router := http.ServeMux{}
 	router.Handle("/status", &GetStatusHandler{MumbleAddress: mumble_address})
+	reg := prometheus.NewRegistry()
+	collector := NewStatusExporter(mumble_address)
+	reg.MustRegister(&collector)
+	router.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{EnableOpenMetrics: true}))
 
 	log.Fatal(http.ListenAndServe(listenAddr, &router))
 }
